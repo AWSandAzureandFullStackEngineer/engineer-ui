d783d98e4fac7cadb591ec4fb096393b167a7c37
AWSandAzureandFullStackEngineer <awsandazurecloudengineer@gmail.com>
2024-02-11 19:57:04 -0600

    add url.

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -5,17 +5,6 @@ const rubik = Rubik({
   subsets: ['latin'],
   variable: '--font-rubik',
 })
-const preloadRubikFont = () => {
-  const link = document.createElement('link');
-  link.rel = 'preload';
-  link.href = 'https://www.engineershubcentral.net/_next/static/media/c22ccc5eb58b83e1-s.p.woff2';
-  link.as = 'font';
-  link.type = 'font/woff2';
-  link.crossOrigin = 'anonymous';
-  document.head.appendChild(link);
-};
-
-preloadRubikFont();
 
 export const fonts = {
   rubik,
