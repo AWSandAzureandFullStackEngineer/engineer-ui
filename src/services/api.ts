de5551a54aacdedcc5c735cd6171d905d6a62cbb
AWSandAzureandFullStackEngineer <awsandazurecloudengineer@gmail.com>
2024-02-13 22:43:57 -0600

    possible fix.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
+const api = axios.create({
+  baseURL: '' // Replace 'your-domain.com' with your actual domain
+});
 
 export const registerUser = async (userData: any) => {
   try {
-    const response = await axios
-      .post('https://engineershubcentral.net/api/1.0/users/register', userData);
+    const response = await api.post('https://engineershubcentral.net/api/1.0/users/register', userData);
     return response.data;
   } catch (error) {
     throw new Error('Failed to register user');
   }
 };
 
+export default api;
